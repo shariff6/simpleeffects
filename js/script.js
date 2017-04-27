5a1e0943c42df1e9618140677f4099065865ef2f
shariff6 <shariff.mohammed.a@gmail.com>
2017-04-27 09:56:22 +0300

    tried out different jquery methods

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-
-      $(document).ready(function() {
-        $(".clickable").click(function() {
-          $(".walrusshowing").toggle();
-          $(".walrushidden").toggle();
-        });
-      });
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrusshowing").toggle();
+    $(".walrushidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clock").click(function() {
+    $(".hiddenclock").toggle();
+    $(".clocks").toggle();
+  })
+})
+$(document).ready(function() {
+  $(".press").click(function() {
+    $(".cake").slideToggle();
+    $(".hidecake").toggle();
+  })
+})
+$(document).ready(function() {
+  $(".money").click(function(){
+    $(".moneys").toggle();
+    $(".hahaha").toggle();
+  })
+})
